f293731c762b941cad4b29efeec47b71facd5d7a
davtur19 <dav.tur19@gmail.com>
2019-08-31 19:00:06 +0200

    Fix input.php
    Ora non risponde più quando non deve (in alcuni casi particolari ritornava true invece di false)
    Rimosse alcune parti inutili
    Sicuramente si può scrivere meglio questo codice, ma funziona bene

--- functions/input.php
+++ functions/input.php
@@ -1,38 +1,46 @@
 <?php
 
-function command($input, $parameters = null)
+function command($input, $parameters = null, $custom = '')
 {
     global $text;
     global $chat_id;
+    $nickname = strtolower(NICKNAMEBOT);
 
     if ($text === '' or $chat_id === 0) {
         return false;
     }
 
+    $nicknameEx = explode($input . '@', $text, 2)[1];
+    if (isset($nicknameEx)) {
+        $nickSplit = preg_split("/\s/", $nicknameEx, 2)[0];
+        if (isset($nickSplit) and !(strcasecmp('@' . $nickSplit, $nickname) === 0)) {
+            return false;
+        }
+    }
+
     if (!isset($parameters)) {
-        if (strcasecmp($text, $input) === 0 or
-            strcasecmp($text, $input . strtolower(NICKNAMEBOT)) === 0 or
-            stripos($text, $input . ' ') === 0 or
-            stripos($text, $input . strtolower(NICKNAMEBOT) . ' ') === 0) {
+        if (stripos($text, trim($input)) === 0) {
             return true;
         }
         return false;
     } elseif (stripos($text, $input) === 0) {
-            if ($parameters === 0) {
-                return true;
-            }
-
-            $save = explode(' ', $text, $parameters + 1);
-            for ($i = 1; $i <= $parameters; $i++) {
-                if (!isset($save[$i]) or $save[$i] == '') {
-                    sendMessage($chat_id,
-                        'Error: unspecified text.' . PHP_EOL .
-                        'How to use:' . PHP_EOL .
-                        $input . str_repeat(' [text]', $parameters));
-                    return false;
-                }
-            }
-            return $save;
+        if ($parameters === 0) {
+            return true;
         }
+
+        $split = preg_split("/\s/", $text, $parameters + 1);
+        for ($i = 1; $i <= $parameters; $i++) {
+            if (!isset($split[$i]) or $split[$i] == '') {
+                $out = 'Error: unspecified text.' . PHP_EOL . 'How to use:' . PHP_EOL . $input . str_repeat(' [text]',
+                        $parameters);
+                if ($custom !== '') {
+                    $out = $custom;
+                }
+                sendMessage($chat_id, $out, 'HTML');
+                return false;
+            }
+        }
+        return $split;
+    }
     return false;
 }
