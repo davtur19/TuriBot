dce8bf84bbc615ccc8bb315e866cb7e2fc0802f0
Davide <dav.tur19@gmail.com>
2022-09-08 03:31:34 +0200

    New InputFile class

---
+++ src/InputFile.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace TuriBot;
+
+use Amp\Http\Client\Body\FileBody;
+
+class InputFile
+{
+    private string $path;
+    private string $filename;
+
+    public function __construct(string $path)
+    {
+        $this->path = $path;
+        $this->filename = basename($path);
+
+    }
+
+    public function getPath(): string
+    {
+        return $this->path;
+    }
+
+    public function getFileName(): string
+    {
+        return $this->filename;
+    }
+}
