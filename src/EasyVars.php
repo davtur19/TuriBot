07b8205d2a0d94e653b233e8a1ed7af157bf1692
Davide <dav.tur19@gmail.com>
2022-09-11 20:02:31 +0200

    Update EasyVars.php

--- src/EasyVars.php
+++ src/EasyVars.php
@@ -4,37 +4,43 @@ namespace TuriBot;
 
 class EasyVars
 {
-    public $type, $chat_type, $chat_id, $message_id, $from_id, $text, $first_name;
+    public string $update_type, $chat_type, $text, $first_name;
 
-    private $types = [
+    public int $chat_id, $message_id, $from_id;
+
+    private array $update_types = [
         "message",
         "edited_message",
         "channel_post",
         "edited_channel_post",
+        "callback_query",
+        "inline_query",
+        "pre_checkout_query",
+        "my_chat_member",
     ];
 
     public function __construct($update)
     {
 
-        foreach ($this->types as $type) {
-            if (isset($update->{$type})) {
-                $this->type = $type;
+        foreach ($this->update_types as $update_type) {
+            if (isset($update->{$update_type})) {
+                $this->update_type = $update_type;
                 break;
             }
         }
 
-        if (isset($this->type)) {
-            $this->chat_type = $update->{$this->type}->chat->type;
-            $this->chat_id = $update->{$this->type}->chat->id;
-            $this->message_id = $update->{$this->type}->message_id;
+        if (isset($this->update_type)) {
+            $this->chat_type = $update->{$this->update_type}->chat->type;
+            $this->chat_id = $update->{$this->update_type}->chat->id;
+            $this->message_id = $update->{$this->update_type}->message_id;
 
-            if (isset($update->{$this->type}->from)) {
-                $this->from_id = $update->{$this->type}->from->id;
-                $this->first_name = $update->{$this->type}->from->first_name;
+            if (isset($update->{$this->update_type}->from)) {
+                $this->from_id = $update->{$this->update_type}->from->id;
+                $this->first_name = $update->{$this->update_type}->from->first_name;
             }
 
-            if (isset($update->{$this->type}->text)) {
-                $this->text = $update->{$this->type}->text;
+            if (isset($update->{$this->update_type}->text)) {
+                $this->text = $update->{$this->update_type}->text;
             }
         }
     }
