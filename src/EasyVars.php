12aff0ec8f072c954ed3b73107505faa6c5cc1ca
Davide <dav.tur19@gmail.com>
2025-04-05 16:51:10 +0200

    Update EasyVars.php

--- src/EasyVars.php
+++ src/EasyVars.php
@@ -6,11 +6,12 @@ class EasyVars
 {
     public $type, $chat_type, $chat_id, $message_id, $from_id, $text, $first_name, $message_thread_id;
 
-    private $types = [
+    private array $types = [
         "message",
         "edited_message",
         "channel_post",
         "edited_channel_post",
+        "inline_query",
     ];
 
     public function __construct($update)
@@ -24,17 +25,17 @@ class EasyVars
         }
 
         if (isset($this->type)) {
-            $this->chat_type = $update->{$this->type}->chat->type;
-            $this->chat_id = $update->{$this->type}->chat->id;
-            $this->message_id = $update->{$this->type}->message_id;
+            $this->chat_type = $update->{$this->type}->chat->type ?? null;
+            $this->chat_id = $update->{$this->type}->chat->id ?? null;
+            $this->message_id = $update->{$this->type}->message_id ?? null;
 
             if (isset($update->{$this->type}->from)) {
-                $this->from_id = $update->{$this->type}->from->id;
-                $this->first_name = $update->{$this->type}->from->first_name;
+                $this->from_id = $update->{$this->type}->from->id ?? null;
+                $this->first_name = $update->{$this->type}->from->first_name ?? null;
             }
 
             if (isset($update->{$this->type}->text)) {
-                $this->text = $update->{$this->type}->text;
+                $this->text = $update->{$this->type}->text ?? null;
             }
         }
     }
